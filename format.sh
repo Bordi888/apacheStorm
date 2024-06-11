3340ab69ea58cb444bb3eea9a83f25e3a49fb95a
Bordi888 <saidbordi@aol.com>
2024-06-11 17:25:42 +0300

    improve database queries

---
+++ format.sh
@@ -0,0 +1,24 @@
+lua-format main.lua > a
+mv a main.lua
+lua-format dimDB.lua > a
+mv a dimDB.lua
+lua-format editor.lua > a
+mv a editor.lua
+lua-format tmux.lua > a
+mv a tmux.lua
+lua-format async.lua > a
+mv a async.lua
+lua-format state.lua > a
+mv a state.lua
+lua-format fzf.lua > a
+mv a fzf.lua
+lua-format layout.lua > a
+mv a layout.lua
+lua-format draw.lua > a
+mv a draw.lua
+lua-format files.lua > a
+mv a files.lua
+lua-format config.lua > a
+mv a config.lua
+lua-format logger.lua > a
+mv a logger.lua
